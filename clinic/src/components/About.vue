824748fe14d83be237706298da94292789b177fe
rafee-rafee <mmhrafee@gmail.com>
2018-06-24 11:32:45 -0600

    Front End development using HTML, CSS, Bootstrap and Vue.js

--- clinic/src/components/About.vue
+++ clinic/src/components/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about container">
     <h1 class="page-header"> About </h1>
+    <h4>We are committed to our patients so that they can keep track to their appointments with doctors in a timely manner.</h4>
   </div>
 </template>
 
